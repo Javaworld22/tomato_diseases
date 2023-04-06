f895674b6946ec448f097a5fa210b92d0e5e10cb
Iheruome Michael <32968231+Javaworld22@users.noreply.github.com>
2023-04-06 22:49:40 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+const bodyParser = require ('body-parser')
+var app = express()
+var path = require('path')
+
+const port = process.env.PORT || 3030
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+app.disable('etag');
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/', (req, res) => {
+    res.render('index');
+  });
+
+  app.listen(port, ()=>{console.log(`Starting the server at port ${port}`)})
